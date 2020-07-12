cee8e3cd933222c801a9de736db7c051203b605e
Omar Polo <op@omarpolo.com>
2020-07-12 18:37:36 +0200

    further simplify the echobot example

--- examples/echobot.clj
+++ examples/echobot.clj
@@ -1,6 +1,6 @@
 (ns echobot
   "This is a minimal example of a bot.  It includes commands, text
-  handling and inline buttons in less than 50 LOC."
+  handling and inline buttons in less than 40 LOC."
   (:require [com.omarpolo.robotto.core :as robotto]
             [com.omarpolo.robotto.effect :as fx]
             [com.omarpolo.robotto.keyboard :as key])
@@ -9,7 +9,7 @@
 (defn on-start [{{{id :id} :chat} :message, :as ctx}]
   (fx/send-message ctx {:chat-id id
                         :text    "Hello there.  This is echobot!"
-                        
+
                         :inline-keyboard
                         [[(key/inline-callback "do not click!" "btn-id")]]}))
 
@@ -23,7 +23,7 @@
                                  :text "am I a joke to you?"
                                  :show-alert? true}))
 
-(defn bot-main []
+(defn -main [& _]
   (let [ctx (-> (robotto/new-ctx)
                 (robotto/set-token (System/getenv "TELEGRAM_TOKEN"))
                 (robotto/on-command :start on-start)
@@ -33,9 +33,6 @@
     (loop [ctx ctx]
       (recur (robotto/get-updates ctx)))))
 
-(defn- main [& _]
-  (bot-main))
-
 (comment
-  (bot-main)
+  (-main)
 )
